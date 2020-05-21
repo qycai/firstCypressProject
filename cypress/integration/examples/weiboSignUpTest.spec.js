6d5583ce44d7ec360a33a87b8fab8cec889ab016
qycai <1071226064@qq.com>
2020-05-21 17:11:07 +0800

    [qunyao]add some visual test code

--- cypress/integration/examples/weiboSignUpTest.spec.js
+++ cypress/integration/examples/weiboSignUpTest.spec.js
@@ -5,11 +5,19 @@ describe('weibo sign up test', () => {
         cy.visit('https://weibo.com/signup/signup.php')
     })
     it('should sign up in weibo successfully', () => {
+        // cy.eyesOpen({
+        //     appName: 'Hello World!',
+        //     testName: 'My first JavaScript test!',
+        //     browser: { width: 800, height: 600 }
+        // })
         signUpPage.inputUserName('123')
+        // cy.eyesCheckWindow('')
         signUpPage.inputPassword('123098')
         signUpPage.selectBirthday('1995', '2', '21')
         signUpPage.inputPinCode('123')
         signUpPage.submit()
+        // cy.eyesCheckWindow('')
+        // cy.eyesClose()
         signUpPage.shouldShowErrorMessage('手机号长度11位，以13/14/15/16/17/18/19开头')
     })
 })
